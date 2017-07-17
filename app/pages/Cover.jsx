0e06c1ee50e4985ccfd0fd1331bd1d1e0c9ded78
Cary Cooper <Cary Cooper>
2017-07-17 03:18:38 -0400

    New Cover Component ready for code from FE

---
+++ app/pages/Cover.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Page from '../pages/Page';
+import CoverContainer from '../containers/Cover';
+
+class Cover extends Component {
+  getMetaData() {
+    return {
+      title: this.pageTitle(),
+      meta: this.pageMeta(),
+      link: this.pageLink()
+    };
+  }
+
+  pageTitle = () => {
+    return 'Cover | Meg\'d';
+  };
+
+  pageMeta = () => {
+    return [
+      { name: 'description', content: 'A reactGo example of a dashboard page' }
+    ];
+  };
+
+  pageLink = () => {
+    return [];
+  };
+
+  render() {
+    return (
+      <Page {...this.getMetaData()}>
+        <CoverContainer {...this.props} />
+      </Page>
+    );
+  }
+}
+
+export default Cover;
