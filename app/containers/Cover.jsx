7324deadd8d7df1b83e8ae5e279764f8723f7752
Cary Cooper <Cary Cooper>
2017-07-20 21:20:25 -0400

    Heroku Update

--- app/containers/Cover.jsx
+++ app/containers/Cover.jsx
@@ -12,18 +12,13 @@ const cx = classNames.bind(styles);
 const Cover = () => {
   return (
     <div className={cx('about')}>
-      <h1 className={cx('header')}>About Ninja Ocean</h1>
+      <h1 className={cx('header')}></h1>
       <div className={cx('description')}>
-        <p>Imagine an ocean of ninjas. Now think of it as a metaphor.<br />
-          Seriously, we love good tech. React, redux, scala, Haskell, machine learning, you name it!
+        <p>
         </p>
       </div>
       <div className={cx('contribute')}>
-        <p>Want to contribute? Help us out!
-          If you think the code on &nbsp;
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/choonkending/react-webpack-node">this repo</a>
-        &nbsp;could be improved, please create an issue&nbsp;
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/choonkending/react-webpack-node/issues">here</a>!
+        <p>
         </p>
       </div>
     </div>
